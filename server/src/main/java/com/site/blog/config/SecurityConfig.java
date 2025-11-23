c1072c55bfd0fddca13ad3c7d316d21cf3a7cc30
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2025-11-23 18:12:09 +0100

    security

--- server/src/main/java/com/site/blog/config/SecurityConfig.java
+++ server/src/main/java/com/site/blog/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.site.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,23 +17,20 @@ public class SecurityConfig {
     @Bean
     @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // csrf is disabled for now, since now only public stateless APIs
-        // enable it if  using server based web forms later
-        // TODO: set cors not set yet, if there is a front end
-
+        // csrf is disabled, since now only public stateless APIs
+        // set cors if there is a front-end later
         http.securityMatcher(PUBLIC_PATHS)
                 .authorizeHttpRequests(
                         authorize -> authorize.anyRequest().permitAll()
-                ).csrf(AbstractHttpConfigurer::disable); // only stateless REST APIs for now
-
+                ).csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
     @Bean
     public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
-                .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
+                .anyRequest().denyAll()
+        );
         return http.build();
     }
 }
