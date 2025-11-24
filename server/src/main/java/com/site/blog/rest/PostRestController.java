818cb55509adb4754e97445fa1256290bd6d4eab
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2025-11-24 17:30:46 +0100

    get mapping for posts

--- server/src/main/java/com/site/blog/rest/PostRestController.java
+++ server/src/main/java/com/site/blog/rest/PostRestController.java
@@ -1,17 +1,33 @@
 package com.site.blog.rest;
 
+import com.site.blog.entity.BlogPost;
+import com.site.blog.service.BlogPostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/posts")
 public class PostRestController {
 
+    private final BlogPostService postService;
+
+    public PostRestController(BlogPostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/test")
     public Map<String, String> sayHello() {
         return Map.of("message", "Hello World!");
     }
+
+    @GetMapping("/")
+    public List<BlogPost> getPosts() {
+        return this.postService.getAllPosts();
+    }
+
+
 }
--- server/src/main/java/com/site/blog/service/BlogPostService.java
+++ server/src/main/java/com/site/blog/service/BlogPostService.java
@@ -3,7 +3,10 @@ package com.site.blog.service;
 import com.site.blog.entity.BlogPost;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -13,6 +16,20 @@ public class BlogPostService {
 
     public BlogPostService() {
         this.blogPostMap = new HashMap<>();
+        generateSampleData();
+    }
+
+    public List<BlogPost> getAllPosts() {
+        return this.blogPostMap.values().stream().sorted(
+                Comparator.comparing(blogPost -> Instant.parse(blogPost.date()))
+        ).toList();
+    }
+
+    private void generateSampleData() {
+        BlogPost post1 = new BlogPost("123", "Jane Doe", "2025-05-16T10:30:00Z", "First post content.");
+        BlogPost post2 = new BlogPost("124", "John Smith", "2025-05-16T11:00:00Z", "Second post content.");
+        this.blogPostMap.put(post1.id(), post1);
+        this.blogPostMap.put(post2.id(), post2);
     }
 
 
