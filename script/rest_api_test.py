0ccc507a5e089b649ca15c16f92163101a876a53
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2025-11-26 08:16:19 +0100

    rewrite test done

--- script/rest_api_test.py
+++ script/rest_api_test.py
@@ -12,25 +12,32 @@ logging.basicConfig(
     datefmt="%H:%M:%S",
 )
 
+# REST API helper functions
+
+
 def get_posts() -> list:
     """List of all posts"""
     response = requests.get(POSTS_URL)
-    logging.info(f"GET Status: {response.status_code}, Response: {response.json()}")
+    logging.info(
+        f"GET Status: {response.status_code}, Response: {response.json()}")
     assert response.status_code == 200
     posts = response.json()
     assert isinstance(posts, list)
     logging.info("GET request successful")
     return posts
 
+
 def get_post(id: str) -> dict:
     """A single posts"""
     response = requests.get(f"{POSTS_URL}/{id}")
-    logging.info(f"GET /postId Status: {response.status_code}, Response: {response.json()}")
+    logging.info(
+        f"GET /postId Status: {response.status_code}, Response: {response.json()}")
     assert response.status_code == 200
     post = response.json()
     assert isinstance(post, dict)
     logging.info("GET /postId request successful")
     return post
+
 
 def delete_post(id: str):
     """Delete a post"""
@@ -39,144 +46,106 @@ def delete_post(id: str):
     assert response.status_code == 204
     logging.info("DELETE request successful")
 
-def create_post(post_data):
+
+def create_post(post_data: dict) -> dict:
     """Create a post"""
     response = requests.post(POSTS_URL, json=post_data)
+    logging.info(
+        f"POST status: {response.status_code}, Response: {response.json()}")
+    assert response.status_code == 201
+    post = response.json()
+    assert "id" in post
+    assert "author" in post
+    assert "date" in post
+    assert "content" in post
+    return post
 
 
+def update_post(id: str, post_data: dict) -> dict:
+    """Update a post"""
+    response = requests.put(f"{POSTS_URL}/{id}", json=post_data)
+    logging.info(
+        f"PUT status: {response.status_code}, Response: {response.json()}")
+    assert response.status_code == 200
+    post = response.json()
+    assert "id" in post
+    assert "author" in post
+    assert "date" in post
+    assert "content" in post
+    return post
+
+
+# Test pre-run setup
 
 @pytest.fixture(scope="function")
 def setup():
     """Ensure tests start with no posts"""
     posts = get_posts()
     if posts:
-        logging.info("Remove existing posts before tests")
+        logging.info("Remove any existing posts before tests")
     for post in posts:
         assert "id" in post
         delete_post(post["id"])
     yield
 
+# Test
+
 
 def test_rest_api(setup):
-    logging.info("---------------------------------------------------------------------")
-    # logging.info("Test GET /posts")
-    # logging.info("Get the example posts in server")
-    # url = f"{BASE_URL}/posts"
-    # response = requests.get(url)
-    # logging.info(f"Status: {response.status_code}")
-    # logging.info(f"Response: {response.json()}")
-    # assert response.status_code == 200
-    # data = response.json()
-    # assert isinstance(data, dict)
-    # assert "description" in data
-    # assert "A list of posts" in data["description"]
-    # assert "posts" in data
-    # assert isinstance(data["posts"], list)
-    # logging.info("Get request successful")
+    logging.info("-----------------------------------------------")
+    logging.info("Test no posts exists")
+    posts = get_posts()
+    assert len(posts) == 0
+    logging.info("Test no posts exists is successful")
 
-    # logging.info("---------------------------------------------------------------------")
-    # logging.info("Delete any existing posts")
+    logging.info("-----------------------------------------------")
+    logging.info("Test create a new post")
+    new_post_data: dict = {
+        "author": "Jackson Dane",
+        "content": "Custom new blog post",
+        "date": "2025-06-16T10:30:00Z"
+    }
+    created_post = create_post(new_post_data)
+    assert created_post["author"] == new_post_data["author"]
+    assert created_post["content"] == new_post_data["content"]
+    assert created_post["date"] == new_post_data["date"]
+    created_post_id = created_post["id"]
+    logging.info("Test creation of a new post is successful")
 
-    # for post in data["posts"]:
-    #     assert "id" in post
-    #     logging.info(f"Deleting post with id {post['id']}")
-    #     response = requests.delete(f"{url}/{post['id']}")
-    #     logging.info(f"Status: {response.status_code}")
-    #     assert response.status_code == 204
-    #     logging.info("Delete request successful")
-    
-    # logging.info("---------------------------------------------------------------------")
-    # logging.info("Create a new post")
+    logging.info("-----------------------------------------------")
+    logging.info("Test get a new post")
+    existing_post = get_post(created_post_id)
+    assert created_post["id"] == existing_post["id"]
+    assert created_post["author"] == existing_post["author"]
+    assert created_post["content"] == existing_post["content"]
+    assert created_post["date"] == existing_post["date"]
+    logging.info("Test get a single post is successful")
 
-    # new_post: dict = {
-    #     "author": "Jackson Dane",
-    #     "content": "Custom new blog post",
-    #     "date": "2025-06-16T10:30:00Z"
-    #     }
-    # logging.info("New post:")
-    # logging.info(new_post)
-    # logging.info("---------------------------------------------------------------------")
-    # logging.info("Test POST /posts")
-    # response = requests.post(url, json=new_post)
-    # logging.info(f"Status: {response.status_code}")
-    # logging.info(f"Response: {response.json()}")
-    # assert response.status_code == 201
-    # data = response.json()
-    # assert isinstance(data, dict)
-    # assert "description" in data
-    # assert "Post created" in data["description"]
-    # assert "post" in data
-    # created_post = data["post"]
-    # assert "id" in created_post
-    # assert "author" in created_post
-    # assert "date" in created_post
-    # assert "content" in created_post
-    # assert created_post["author"] == new_post["author"] 
-    # assert created_post["content"] == new_post["content"] 
-    # assert created_post["date"] == new_post["date"]
-    # id_created_post = created_post["id"]
-    # logging.info("Post request successful")
-    # logging.info("---------------------------------------------------------------------")
-    # logging.info("Test PUT /posts/id")
-    # update_post: dict = {
-    #     "author": "Jackson Dane",
-    #     "content": "Updated the blog post",
-    #     "date": "2025-07-16T11:30:00Z"
-    #     }
-    # logging.info("Update post:")
-    # logging.info(update_post)
-    # response = requests.put(f"{url}/{id_created_post}", json=update_post)
-    # logging.info(f"Status: {response.status_code}")
-    # logging.info(f"Response: {response.json()}")
-    # assert response.status_code == 200
-    # data = response.json()
-    # assert isinstance(data, dict)
-    # assert "description" in data
-    # assert "Post updated" in data["description"]
-    # assert "post" in data
-    # updated_post = data["post"]
-    # assert "id" in updated_post
-    # assert "author" in updated_post
-    # assert "date" in updated_post
-    # assert "content" in updated_post
-    # assert updated_post["id"] == id_created_post
-    # assert updated_post["author"] == update_post["author"] 
-    # assert updated_post["content"] == update_post["content"] 
-    # assert updated_post["date"] == update_post["date"]
-    # logging.info("---------------------------------------------------------------------")
-    # logging.info("Test GET /posts")
-    # response = requests.get(url)
-    # logging.info(f"Status: {response.status_code}")
-    # logging.info(f"Response: {response.json()}")
-    # assert response.status_code == 200
-    # data = response.json()
-    # assert isinstance(data, dict)
-    # assert "description" in data
-    # assert "A list of posts" in data["description"]
-    # assert "posts" in data
-    # assert isinstance(data["posts"], list)
-    # assert len(data["posts"]) == 1
-    # post = data["posts"][0]
-    # assert "id" in post
-    # assert "author" in post
-    # assert "date" in post
-    # assert "content" in post
-    # assert updated_post["id"] == post["id"]
-    # assert updated_post["author"] == post["author"] 
-    # assert updated_post["content"] == post["content"] 
-    # assert updated_post["date"] == post["date"]
-    # logging.info("Get request successful")
-    # logging.info("---------------------------------------------------------------------")
-    # logging.info("Test DELETE /posts/id")
-    # logging.info(f"Deleting post with id {post['id']}")
-    # response = requests.delete(f"{url}/{post['id']}")
-    # logging.info(f"Status: {response.status_code}")
-    # assert response.status_code == 204
-    # logging.info("Delete request successful")
-    
+    logging.info("-----------------------------------------------")
+    logging.info("Test update a post")
+    update_post_data: dict = {
+        "author": "J Dane",
+        "content": "Updated the blog post",
+        "date": "2025-07-16T11:30:00Z"
+    }
+    updated_post = update_post(created_post_id, update_post_data)
+    assert updated_post["id"] == created_post_id
+    assert updated_post["author"] == update_post_data["author"]
+    assert updated_post["content"] == update_post_data["content"]
+    assert updated_post["date"] == update_post_data["date"]
+    logging.info("Test update a post is successful")
 
+    logging.info("-----------------------------------------------")
+    logging.info("Test get all posts to check on the updated post")
+    posts = get_posts()
+    assert len(posts) == 1
+    assert updated_post == posts[0]
+    logging.info("Test get all posts to check the updated post is successful")
 
-    
-
-
-    
+    logging.info("-----------------------------------------------")
+    logging.info("Test delete the post")
+    delete_post(created_post_id)
+    posts = get_posts()
+    assert len(posts) == 0
+    logging.info("Test delete the post is successful")
+    logging.info("-----------------------------------------------")
